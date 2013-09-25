a11b863854a83ec06e1321c376b693a91473bca7
Saravanan Ganesh <srrvnn@live.com>
2013-09-25 12:10:33 -0700

    lists - base and remove function

---
+++ lists/list.java
@@ -0,0 +1,137 @@
+/*
+
+The best way to implement a linked list in Java is this way: 
+LinkedList list_objects = new java.util.LinkedList();
+
+The following code is for learning purposes only. 
+
+*/
+
+public class List {
+
+	// node object declared inside List. 
+
+	private class Node {
+
+		private Object data; 
+
+		private Node next; 
+		private Node prev;
+
+		public Node(Object data, Node next, Node prev){
+
+			this.data = data;
+			this.next = next; 
+			this.prev = prev;
+		}
+
+		public Object data(){
+
+			return data; 
+		}		
+
+		public Object next(){
+
+			return this.next;
+		}
+
+		public Object prev(){
+
+			return this.prev;
+		}
+
+		public void set(Object data){
+
+			this.data = data; 
+		}
+	}
+
+	// data members - start node. 
+
+	private Node head; 
+	private Node tail;
+
+	private int size; 
+
+	// data functions - add, remove, contains, data, set, isEmpty, size. 
+
+	public boolean add(){
+
+
+	}
+
+	public boolean remove(Object data){
+
+		// check for empty list
+
+		if(isEmpty())
+			return false;
+
+		// start at the head
+
+		Node parent = this.head; 
+		Node current = this.head;
+
+		// iterate through the loop
+
+		while(current.data != data){			
+
+			// reached the end?
+
+			if(current.next() == null){
+
+				return false;
+			}		
+
+			// or move forward
+
+			parent = current;
+			current = current.next();
+		}
+
+		// reposition head and tail if they are deleted
+
+		if(current == this.head)
+			this.head = current.next();
+
+		if(current == this.tail)
+			this.tail = current.next();
+
+		// delete the node
+
+		parent.next() = current.next();		
+		delete current;
+
+		return true;		
+	}
+
+	public boolean contains(Object data){
+
+		Node iterator = this.head; 
+
+		while(iterator != tail){
+
+			if(iterator.data() == data)
+				return true; 
+
+			iterator = iterator.next();							
+		}
+
+		return false;
+	}	
+
+	public boolean isEmpty(){
+
+		return(size==0);
+	}
+
+	public int size(){
+
+		return this.size;
+	}
+
+	// detect loop in a linked lists 
+
+	public boolean isLooping()
+
+}
