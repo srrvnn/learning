b1af3e4967e6c3a154f5d901ffc77acb454fb22c
srrvnn <srrvnn@live.com>
2013-10-13 15:43:15 -0700

    testing commit from Ubuntu

---
+++ trees/Tree.java
@@ -0,0 +1,19 @@
+public class Tree{
+
+
+
+	public static void main(String[] args){
+
+
+	}
+
+	// NESTED CLASSES
+
+	public class Node{
+
+		private int data;
+
+		private Node left;
+		private Node right
+	}
+}
